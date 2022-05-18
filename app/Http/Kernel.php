bb4abb8271b694493949ee47980514ac4d9b73a0
MatusVetrik1 <matus.vetrik@iteria.io>
2022-05-18 12:46:34 +0200

    fix: fix cors, move routes to api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \App\Http\Middleware\BasicAuth::class
+        \App\Http\Middleware\BasicAuth::class,
     ];
 
     /**
