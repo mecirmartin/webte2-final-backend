2c02c4b923c19d6824658300f641d4dd9c3c2f71
mecirmartin <mecir.martin@gmail.com>
2022-05-20 18:57:12 +0200

    feat: logging

--- routes/web.php
+++ routes/web.php
@@ -30,26 +30,38 @@ function custom_shell_exec($cmd, &$stdout = null, &$stderr = null)
 function get_log($commands, $stdout, $stderr)
 {
     $date = date('m/d/Y h:i:s a', time());
-    $status = $stdout ? "OK" : "ERROR";
-    $output = $stdout ? $stdout : $stderr;
+    $status = $stdout != null ? "OK" : "ERROR";
+    $output = $stdout != null ? $stdout : $stderr;
 
     return "$date, $commands, $status, $output";
 }
 
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+    $r = $request->input('r');
+    $init = $request->input('initialValues');
+
     $stdout = '';
     $stderr = '';
-    custom_shell_exec('octave-cli --eval "m1 = 2500; m2 = 320;k1 = 80000; k2 = 500000;b1 = 350; b2 = 15020;pkg load control;A=[0 1 0 0;-(b1*b2)/(m1*m2) 0 ((b1/m1)*((b1/m1)+(b1/m2)+(b2/m2)))-(k1/m1) -(b1/m1);b2/m2 0 -((b1/m1)+(b1/m2)+(b2/m2)) 1;k2/m2 0 -((k1/m1)+(k1/m2)+(k2/m2)) 0];B=[0 0;1/m1 (b1*b2)/(m1*m2);0 -(b2/m2);(1/m1)+(1/m2) -(k2/m2)];C=[0 0 1 0]; D=[0 0];Aa = [[A,transpose([0 0 0 0])];[C, 0]];Ba = [B;[0 0]];Ca = [C,0]; Da = D;K = [0 2.3e6 5e8 0 8e6];sys = ss(Aa-Ba(:,1)*K,Ba,Ca,Da);t = 0:0.01:5;r =0.1;initX1=0; initX1d=0;initX2=0; initX2d=0;[y,t,x]=lsim(sys*[0;1],r*ones(size(t)),t,[initX1;initX1d;initX2;initX2d;0]);save x3.txt y; save x1.txt x(:,1); save t.txt t; disp([t, x(:,1), y]);"', $stdout, $stderr);
-    $log = get_log('octave-cli --eval "m1 = 2500; m2 = 320;k1 = 80000; k2 = 500000;b1 = 350; b2 = 15020;pkg load control;A=[0 1 0 0;-(b1*b2)/(m1*m2) 0 ((b1/m1)*((b1/m1)+(b1/m2)+(b2/m2)))-(k1/m1) -(b1/m1);b2/m2 0 -((b1/m1)+(b1/m2)+(b2/m2)) 1;k2/m2 0 -((k1/m1)+(k1/m2)+(k2/m2)) 0];B=[0 0;1/m1 (b1*b2)/(m1*m2);0 -(b2/m2);(1/m1)+(1/m2) -(k2/m2)];C=[0 0 1 0]; D=[0 0];Aa = [[A,transpose([0 0 0 0])];[C, 0]];Ba = [B;[0 0]];Ca = [C,0]; Da = D;K = [0 2.3e6 5e8 0 8e6];sys = ss(Aa-Ba(:,1)*K,Ba,Ca,Da);t = 0:0.01:5;r =0.1;initX1=0; initX1d=0;initX2=0; initX2d=0;[y,t,x]=lsim(sys*[0;1],r*ones(size(t)),t,[initX1;initX1d;initX2;initX2d;0]);save x3.txt y; save x1.txt x(:,1); save t.txt t; disp([t, x(:,1), y]);");', $stdout, $stderr);
+
+    custom_shell_exec("octave-cli --eval \"m1 = 2500; m2 = 320;k1 = 80000; k2 = 500000;b1 = 350; b2 = 15020;pkg load control;A=[0 1 0 0;-(b1*b2)/(m1*m2) 0 ((b1/m1)*((b1/m1)+(b1/m2)+(b2/m2)))-(k1/m1) -(b1/m1);b2/m2 0 -((b1/m1)+(b1/m2)+(b2/m2)) 1;k2/m2 0 -((k1/m1)+(k1/m2)+(k2/m2)) 0];B=[0 0;1/m1 (b1*b2)/(m1*m2);0 -(b2/m2);(1/m1)+(1/m2) -(k2/m2)];C=[0 0 1 0]; D=[0 0];Aa = [[A,transpose([0 0 0 0])];[C, 0]];Ba = [B;[0 0]];Ca = [C,0]; Da = D;K = [0 2.3e6 5e8 0 8e6];sys = ss(Aa-Ba(:,1)*K,Ba,Ca,Da);t = 0:0.01:5;r =\"$r\";initX1=0; initX1d=0;initX2=0; initX2d=0;[y,t,x]=lsim(sys*[0;1],r*ones(size(t)),t,[\"$init[0]\";\"$init[1]\";\"$init[2]\";\"$init[3]\";0]);save x3.txt y; save x1.txt x(:,1); save t.txt t; disp([t, x(:,1), y]);\"", $stdout, $stderr);
+    $log = get_log("m1 = 2500; m2 = 320;k1 = 80000; k2 = 500000;b1 = 350; b2 = 15020;pkg load control;A=[0 1 0 0;-(b1*b2)/(m1*m2) 0 ((b1/m1)*((b1/m1)+(b1/m2)+(b2/m2)))-(k1/m1) -(b1/m1);b2/m2 0 -((b1/m1)+(b1/m2)+(b2/m2)) 1;k2/m2 0 -((k1/m1)+(k1/m2)+(k2/m2)) 0];B=[0 0;1/m1 (b1*b2)/(m1*m2);0 -(b2/m2);(1/m1)+(1/m2) -(k2/m2)];C=[0 0 1 0]; D=[0 0];Aa = [[A,transpose([0 0 0 0])];[C, 0]];Ba = [B;[0 0]];Ca = [C,0]; Da = D;K = [0 2.3e6 5e8 0 8e6];sys = ss(Aa-Ba(:,1)*K,Ba,Ca,Da);t = 0:0.01:5;r =\"$r\";initX1=0; initX1d=0;initX2=0; initX2d=0;[y,t,x]=lsim(sys*[0;1],r*ones(size(t)),t,[\"$init[0]\";\"$init[1]\";\"$init[2]\";\"$init[3]\";0]);save x3.txt y; save x1.txt x(:,1); save t.txt t; disp([t, x(:,1), y]);", $stdout, $stderr);
     file_put_contents('logs.csv', $log . PHP_EOL, FILE_APPEND | LOCK_EX);
 
-    return $stdout;
+    return $stdout != null ? $stdout : $stderr;
 });
 
 Route::get('/calculation', function (Request $request) {
     $command = $request->input('command');
 
-    $result = shell_exec("octave-cli --eval \"$command\"");
-    return $result;
+
+    $stdout = '';
+    $stderr = '';
+
+
+    custom_shell_exec("octave-cli --eval \"$command\"", $stdout, $stderr);
+    $log = get_log($command, $stdout, $stderr);
+    file_put_contents('logs.csv', $log . PHP_EOL, FILE_APPEND | LOCK_EX);
+
+    return $stdout != null ? $stdout : $stderr;
 });
